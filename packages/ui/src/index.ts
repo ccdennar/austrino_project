b689aad521e3f15b581f12a675eb6ef658e72d79
Chigaemezu Dennar <salientcrossing@outlook.com>
2026-01-06 19:10:29 -0700

    fix

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,4 @@
-// packages/ui/src/index.ts
-export { HeroCarousel } from './Carousel/HeroCarousel';
+export { HeroCarousel } from './Carousel/HeroCarousel'
+export { Button } from './Form/Button'
+export { Input } from './Form/Input'
+export { Textarea } from './Form/Textarea'
