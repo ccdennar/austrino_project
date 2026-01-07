37198846118145314028a9bc57fa27557fabd38c
Chigaemezu Dennar <salientcrossing@outlook.com>
2026-01-06 22:51:13 -0700

    Carousel

--- packages/ui/src/Carousel/HeroCarousel.tsx
+++ packages/ui/src/Carousel/HeroCarousel.tsx
@@ -1,1 +1,23 @@
-export default function HeroCarousel() { }
+import React from 'react';
+
+interface Props {
+  slides: Array<{
+    title: string;
+    subtitle?: string;
+    image: string;
+  }>;
+}
+
+export default function HeroCarousel({ slides }: Props) {
+  return (
+    <div>
+      {slides.map((s, i) => (
+        <div key={i}>
+          <h2>{s.title}</h2>
+          {s.subtitle && <p>{s.subtitle}</p>}
+          <img src={s.image} alt={s.title} />
+        </div>
+      ))}
+    </div>
+  );
+}
